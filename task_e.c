8d3f741f8fa9d62e31f8472d7c42c09d631caf8b
Yufu-W <160229997+Yufu-W@users.noreply.github.com>
2024-05-06 15:20:58 +0100

    Add files via upload

---
+++ task_e.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(int argc, char* argv[])
+{
+	// Enter your code under here to read the filename from the command line
+	if (argc != 2) {
+		printf("Error. Expected 1 filename.");
+		return 1; 
+	}
+	// Read number from file - do not edit
+	double num;
+	FILE* fptr;
+	fptr = fopen(argv[1], "r");
+	if (fptr == NULL) {
+		 printf("Error. Cannot open requested file.");
+		 return 1;
+		 }
+	fscanf(fptr, "%lf", &num);
+	fclose(fptr);
+
+	// Enter your code from Lab D to print Json output (with name, email, number1, number2) goes under here
+	
+	char* name = "yufu Wang";
+	char* email = "yufu.wang@student.manchester.ac.uk";
+
+	// Output JSON formatted text
+	printf("{\"name\":\"%s\",\"email\":\"%s\",\"number1\":%.2f,\"number2\":%.3e}\n", name, email, num, num);
+	
+	// Do not edit below here
+
+
+	return 0;
+}
